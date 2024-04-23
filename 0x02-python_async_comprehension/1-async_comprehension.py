e7f4df1710cc6ffde4a3d9f8299f610af59cb723
Josephdada01 <ridwandada500@gmail.com>
2024-04-23 13:22:52 +0100

    task completed

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,10 @@
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> list[float]:
     """
     The coroutine will collect 10 random numbers using an async
     an async comprehensing over async_generator, then return the
     10 random numbers.
     """
-    random_numbers = [num async for num in async_generator()]
-    return random_numbers
+    return  [num async for num in async_generator()]
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Run time for four parallel comprehensions"""
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    coroutine that will execute async_comprehension
+    four times in parallel using asyncio.gather.
+    measure_runtime should measure the total
+    runtime and return it.
+    """
+    start_time = asyncio.get_running_loop().time()
+    await asyncio.gather(async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    end_time = asyncio.get_running_loop().time()
+    return end_time - start_time
