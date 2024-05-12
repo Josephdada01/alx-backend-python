b72de505c80fd1bd5be0217e29bcbab3d00c0435
Josephdada01 <ridwandada500@gmail.com>
2024-05-12 22:16:58 +0100

    test memoization

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -57,5 +57,27 @@ class TestGetJson(unittest.TestCase):
             req_get.assert_called_once_with(test_url)
 
 
+class TestMemoize(unittest.TestCase):
+    """Tests the memoize function."""
+    def test_memoize(self) -> None:
+        """Tests memoize's output."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_func:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_func.assert_called_once()
+
+
 if __name__ == '__main__':
     unittest.main()
